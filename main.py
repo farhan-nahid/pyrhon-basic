80ebba53a0a5370ac0acaa796a419b21659b64a9
farhan-nahid <nahidahmed9933@gmail.com>
2021-06-27 21:12:20 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,26 @@
+# print("Farhan Ahmed Nahid")
+# print(20)
+
+# --------  Variable   -------
+
+name = 'Farhan Ahmed Nahid'
+age = 20
+wannaDate = False
+# print(name)
+# print(age)
+# print(wannaDate)
+
+# -----  Mathematical Operator -----
+
+# print(6 + 9)
+# print(15 / 3)
+
+monkey = 3
+bananas = 17
+result = bananas % monkey
+# print(result)
+
+# ------ Input ------
+
+status = input("Are you Single? :")
+print("I am : ", status)
